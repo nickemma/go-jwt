7413694712eb3fec873295db88625b6c08a2f6ac
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-04-13 17:52:27 +0100

    Fixed version error

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nickemma/go-jwt/initializers"
+)
+
+func init() {
+  initializers.LoadEnvVariables()
+		initializers.ConnectToDb()
+		initializers.SyncDatabase()
+}
 
 func main() {
-	fmt.Println("Hey There")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
